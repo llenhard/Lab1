ec2557f0b68ec4e4b3bf86d0c8169de36cc7947c
imsxz <lukelenhard@gmail.com>
2019-04-23 10:44:18 -0400

    lab1

---
+++ ClassArea/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length;
+            double width;
+            double area;
+            double perimeter;
+            bool continuing = true;
+
+            while (continuing)
+            {
+                Console.WriteLine("Enter the length of the room: ");
+                length = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the width of the room: ");
+                width = double.Parse(Console.ReadLine());
+
+                area = length * width;
+                perimeter = (2 * length) + (2 * width);
+
+                Console.WriteLine("Area: {0}\n" +
+                    "Perimeter: {1}\n" +
+                    "Do you want to calculate another room? (Y/N)", area, perimeter);
+
+                if (Console.ReadLine() == "N")
+                {
+                    continuing = false;
+                }
+            }
+            
+        }
+    }
+}
